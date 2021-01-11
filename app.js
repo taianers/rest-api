32fc2a8831e61e75825c965703dac43f6fac7303
Taiane Rodrigues <taianerodrigues.tec99@gmail.com>
2021-01-11 17:40:43 -0300

    tratamento de erro

--- app.js
+++ app.js
@@ -7,4 +7,20 @@ const rotaPedidos = require('./routes/pedidos')
 app.use('/produtos', rotaProdutos)
 app.use('/pedidos', rotaPedidos)
 
+//não encontrou rota
+app.use((req, res, next) => {
+    const erro = new Error('Não encontrado');
+    erro.status = 404;
+    next(erro);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    return res.send({
+        erro: {
+            mensagem: error.message
+        }
+    });
+});
+
 module.exports = app;
--- server.js
+++ server.js
@@ -3,3 +3,4 @@ const app = require('./app');
 const port = process.env.PORT || 3000;
 const server = http.createServer(app);
 server.listen(port);
+console.log(`Server rodando na porta ${port}`)
