eaf14a81199d1f0af3791a5fb3035562e4904906
Taiane Rodrigues <taianerodrigues.tec99@gmail.com>
2021-01-13 13:15:27 -0300

    criado o crud

--- routes/produtos.js
+++ routes/produtos.js
@@ -4,32 +4,29 @@ const mysql = require('../mysql').pool;
 
 //retorna todos os produtos
 router.get('/', (req, res, next) => {
-    res.status(200).send({
-        mensagem: 'Todos os produtos'
+    mysql.getConnection((error, comn) => {
+        if (error) { return res.status(500).send({ error: error }) }
+        comn.query(
+            'SELECT * FROM produtos',
+            (error, resultado, fields) => {   
+                if (error) { return res.status(500).send({ error: error }) }
+                return res.status(200).send({response: resultado})
+            }
+        )
     });
 });
 
 //insere um produto
 router.post('/', (req, res, next) => {
-    
-    /*const produto = {
-        nome: req.body.nome,
-        preco: req.body.preco
-    };*/
-
     mysql.getConnection((error, comn) => {
+        if (error) { return res.status(500).send({ error: error }) }
         comn.query(
             'INSERT INTO produtos (nome, preco) VALUES (?, ?)',
             [req.body.nome, req.body.preco],
             (error, resultado, field) => {
-                comn.release();
+                comn.release(); 
+                if (error) { return res.status(500).send({ error: error }) }
                 
-                if (error){
-                    return res.status(500).send({
-                        error: error,
-                        response: null
-                    });
-                }
                 res.status(201).send({
                     mensagem: 'Produto inserido com sucesso',
                     id_produto: resultado.insertId
@@ -40,31 +37,54 @@ router.post('/', (req, res, next) => {
 });
 
 //retorno os dados de um produto
-router.get('/:id_produto', (req, res, next) => {
-    const id = req.params.id_produto;
-    if (id === '45') {
-        res.status(200).send({
-            mensagem: 'Você passou o ID especial',
-            id: id
-        });
-    } else {
-        res.status(200).send({
-            mensagem: 'Você passou um ID'
-        });
-    }
+router.get('/:id_produtos', (req, res, next) => {
+    mysql.getConnection((error, comn) => {
+        if (error) { return res.status(500).send({ error: error }) }
+        comn.query(
+            'SELECT * FROM produtos WHERE id_produtos = ?;',
+            [req.params.id_produtos],
+            (error, resultado, fields) => {   
+                if (error) { return res.status(500).send({ error: error }) }
+                return res.status(200).send({response: resultado})
+            }
+        )
+    });
 });
 
 //altera um produto
 router.patch('/', (req, res, next) => {
-    res.status(201).send({
-        mensagem: 'Produto alterado'
+    mysql.getConnection((error, comn) => {
+        if (error) { return res.status(500).send({ error: error }) }
+        comn.query(
+            'UPDATE produtos SET nome = ?, preco = ? WHERE id_produtos = ?',
+            [req.body.nome, req.body.preco, req.body.id_produtos],
+            (error, resultado, field) => {
+                comn.release(); 
+                if (error) { return res.status(500).send({ error: error }) }
+                
+                res.status(202).send({
+                    mensagem: 'Produto alterado com sucesso'
+                });
+            }
+        )
     });
 });
 
 //exclui um produto
 router.delete('/', (req, res, next) => {
-    res.status(201).send({
-        mensagem: 'Produto excluído'
+    mysql.getConnection((error, comn) => {
+        if (error) { return res.status(500).send({ error: error }) }
+        comn.query(
+            'DELETE FROM produtos WHERE id_produtos = ?', [req.body.id_produtos],
+            (error, resultado, field) => {
+                comn.release(); 
+                if (error) { return res.status(500).send({ error: error }) }
+                
+                res.status(202).send({
+                    mensagem: 'Produto removido com sucesso'
+                });
+            }
+        )
     });
 });
 
